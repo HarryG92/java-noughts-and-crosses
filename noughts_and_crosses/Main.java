752ccba6c3cbde28191c43ecb2ef0b737ab13883
HarryG92 <gulliver.harry@gmail.com>
2022-01-14 07:49:11 +0000

    Introduce staggered learning. Seems to produce strong first move players, weak second move

--- noughts_and_crosses/Main.java
+++ noughts_and_crosses/Main.java
@@ -13,28 +13,73 @@ public class Main {
 //		selector.adjustOdds(move, 3, 2);
 //		selector.printOdds();
 		
-		ReinforcementPlayer noughts, crosses;
-		noughts = new ReinforcementPlayer();
-		crosses = new ReinforcementPlayer();
-		PlayerInterface[] players = {noughts, crosses};
-		
-		for (int i = 0; i < 10000; i++) {
-			try {
-				Game game = new Game(players);
-				game.runGame();
-			} catch (PlayerNumberException e) {
-				System.out.println("wtf? that's not meant to happen!");
-			}
-			
+		int numPlayers = 10;
+		PlayerInterface[] players = new PlayerInterface[numPlayers];
+		for (int i = 0; i < numPlayers; i++) {
+			players[i] = new ReinforcementPlayer();
 		}
 		
+		int numCurrentPlayers = 2;
+		int numRounds = 100;
+		
+		while (numCurrentPlayers <= numPlayers) {
+			for (int round = 0; round < numRounds; round++) {
+				// one complete round of training
+				for (int i = 0; i < numCurrentPlayers; i++) {
+					for (int j = 0; j < numCurrentPlayers; j++) {
+						PlayerInterface currentPlayers[] = {players[i], players[j]};
+						try {
+							Game game = new Game(currentPlayers);
+							game.runGame();
+						} catch (PlayerNumberException e) {
+							System.out.println("wtf? that's not meant to happen!");
+						}
+					}
+				}
+			}
+			// add 2 new players for the next round
+			numCurrentPlayers += 2;
+			System.out.println(numCurrentPlayers);
+		}
+		
+		// once initial staggered rounds are done, run several full round
+		numCurrentPlayers = numPlayers;
+		numRounds = 1000;
+		for (int round = 0; round < numRounds; round++) {
+			// one complete round of training
+			for (int i = 0; i < numCurrentPlayers; i++) {
+				for (int j = 0; j < numCurrentPlayers; j++) {
+					PlayerInterface currentPlayers[] = {players[i], players[j]};
+					try {
+						Game game = new Game(currentPlayers);
+						game.runGame();
+					} catch (PlayerNumberException e) {
+						System.out.println("wtf? that's not meant to happen!");
+					}
+				}
+			}
+		}
+		
+		
+		
+		
+//		for (int i = 0; i < 100; i++) {
+//			try {
+//				Game game = new Game(players);
+//				game.runGame();
+//			} catch (PlayerNumberException e) {
+//				System.out.println("wtf? that's not meant to happen!");
+//			}
+//			
+//		}
+		
 		HumanPlayer me = new HumanPlayer("H");
-		players[1] = me;
+		PlayerInterface[] currentPlayers = {players[numPlayers - 1], me};
 		boolean cont = true;
 		while (cont) {
 			try {
-				Game game = new Game(players);
-				game.runGame();
+				Game game = new Game(currentPlayers);
+				game.runGame(true);
 			} catch (PlayerNumberException e) {
 				System.out.println("wtf? that's not meant to happen!");
 			}
@@ -50,14 +95,14 @@ public class Main {
 		
 		System.out.println("Change roles");
 		
-		players[1] = crosses;
-		players[0] = me;
+		currentPlayers[0] = me;
+		currentPlayers[1] = players[numPlayers - 1];
 		
 		cont = true;
 		while (cont) {
 			try {
-				Game game = new Game(players);
-				game.runGame();
+				Game game = new Game(currentPlayers);
+				game.runGame(true);
 			} catch (PlayerNumberException e) {
 				System.out.println("wtf? that's not meant to happen!");
 			}
